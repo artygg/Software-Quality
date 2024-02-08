7fefce1cf05a3ac8f06f02a408bfd30dc88560ac
Martijn Pomp <34094994+martijnpomp@users.noreply.github.com>
2024-02-08 10:27:20 +0100

    Update DemoPresentation.java with English language

--- src/DemoPresentation.java
+++ src/DemoPresentation.java
@@ -1,4 +1,4 @@
-/** Een ingebouwde demo-presentatie
+/** A built in demo-presentation
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -15,40 +15,40 @@ class DemoPresentation extends Accessor {
 		Slide slide;
 		slide = new Slide();
 		slide.setTitle("JabberPoint");
-		slide.append(1, "Het Java Presentatie Tool");
+		slide.append(1, "The Java Presentation Tool");
 		slide.append(2, "Copyright (c) 1996-2000: Ian Darwin");
 		slide.append(2, "Copyright (c) 2000-now:");
-		slide.append(2, "Gert Florijn en Sylvia Stuurman");
-		slide.append(4, "JabberPoint aanroepen zonder bestandsnaam");
-		slide.append(4, "laat deze presentatie zien");
-		slide.append(1, "Navigeren:");
-		slide.append(3, "Volgende slide: PgDn of Enter");
-		slide.append(3, "Vorige slide: PgUp of up-arrow");
-		slide.append(3, "Stoppen: q or Q");
+		slide.append(2, "Gert Florijn andn Sylvia Stuurman");
+		slide.append(4, "Starting JabberPoint without a filename");
+		slide.append(4, "shows this presentation");
+		slide.append(1, "Navigate:");
+		slide.append(3, "Next slide: PgDn or Enter");
+		slide.append(3, "Previous slide: PgUp or up-arrow");
+		slide.append(3, "Quit: q or Q");
 		presentation.append(slide);
 
 		slide = new Slide();
-		slide.setTitle("Demonstratie van levels en stijlen");
+		slide.setTitle("Demonstration of levels and stijlen");
 		slide.append(1, "Level 1");
 		slide.append(2, "Level 2");
-		slide.append(1, "Nogmaals level 1");
-		slide.append(1, "Level 1 heeft stijl nummer 1");
-		slide.append(2, "Level 2 heeft stijl nummer 2");
-		slide.append(3, "Zo ziet level 3 er uit");
-		slide.append(4, "En dit is level 4");
+		slide.append(1, "Again level 1");
+		slide.append(1, "Level 1 has style number 1");
+		slide.append(2, "Level 2 has style number  2");
+		slide.append(3, "This is how level 3 looks like");
+		slide.append(4, "And this is level 4");
 		presentation.append(slide);
 
 		slide = new Slide();
-		slide.setTitle("De derde slide");
-		slide.append(1, "Om een nieuwe presentatie te openen,");
-		slide.append(2, "gebruik File->Open uit het menu.");
+		slide.setTitle("The third slide");
+		slide.append(1, "To open a new presentation,");
+		slide.append(2, "use File->Open from the menu.");
 		slide.append(1, " ");
-		slide.append(1, "Dit is het einde van de presentatie.");
+		slide.append(1, "This is the end of the presentation.");
 		slide.append(new BitmapItem(1, "JabberPoint.jpg"));
 		presentation.append(slide);
 	}
 
 	public void saveFile(Presentation presentation, String unusedFilename) {
-		throw new IllegalStateException("Save As->Demo! aangeroepen");
+		throw new IllegalStateException("Save As->Demo! called");
 	}
 }
